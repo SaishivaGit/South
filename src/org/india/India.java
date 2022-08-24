32c8d994428c20ec1a751dff4490c4a0c4462992
SaiShivaGit <selvashiva1820@gmail.com>
2022-08-24 17:30:00 +0530

    south india project

---
+++ src/org/india/India.java
@@ -0,0 +1,18 @@
+package org.india;
+
+import org.andrapradesh.AndraPradesh;
+
+public class India extends AndraPradesh{
+	public void india() {
+		System.out.println("South India Languages");
+
+	}
+	public static void main(String[] args) {
+		India i=new India();
+		i.india();
+		i.tamilLanguage();
+		i.malayalam();
+		i.telugu();
+	}
+
+}
